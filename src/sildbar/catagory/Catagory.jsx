c56acdd8902c6b4006575b934f47aa6af8633e15
Sokpiseth Nhoeun <ahboy5518@gmail.com>
2025-10-22 09:50:21 +0700

    update

--- src/sildbar/catagory/Catagory.jsx
+++ src/sildbar/catagory/Catagory.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Catagory.css";
-import Input from "../../components/input";
+import Input from "../../components/Input.jsx";
 function Catagory({handleChange}) {
  
   return (
